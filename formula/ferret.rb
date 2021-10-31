03459cb103ac399d760db132d2543983c25ae551
aviatam <45319244+aviatam@users.noreply.github.com>
2021-10-31 17:39:19 +0200

    Update ferret.rb

--- formula/ferret.rb
+++ formula/ferret.rb
@@ -4,7 +4,7 @@ require_relative "lib/private_strategy"
 class Ferret < Formula
   desc ""
   homepage "https://github.com/datorama/ferret"
-  url "https://github.com/datorama/ferret/releases/download/1.0.12/ferret-mac-1.0.12.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/salesforce/ferret/releases/download/1.0.12/ferret-mac-1.0.12.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   version "1.0.12"
   sha256 "e1232083d85d5f0bbb904dbeea9e8a9eeb0b0538a0fd23aab46cb7d97c2afbb1"
 
